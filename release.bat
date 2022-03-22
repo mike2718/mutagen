ce8efbb6bbc4234a8c8229ef5d2a4596db162bbd
Mike Akiba <mike2718@gmail.com>
2022-03-23 03:03:05 +0800

    尝试更改帮助文件的默认字体

--- release.bat
+++ release.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-del /q ../mutagen_version_1.23.zip 2>nul
-git archive -v --format=zip --prefix=mutagen_version_1.23/ --output=../mutagen_version_1.23.zip HEAD
+del /q ../mutagen_v1.23.zip 2>nul
+git archive -v --format=zip --prefix=mutagen_v1.23/ --output=../mutagen_v1.23.zip HEAD
