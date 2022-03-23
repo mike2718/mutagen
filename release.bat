7a738d41deb92321c35b48be6da628aada44b436
Mike Akiba <mike2718@gmail.com>
2022-03-24 00:06:08 +0800

    更新文件以匹配version 1.24

--- release.bat
+++ release.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-del /q ../mutagen_v1.23.zip 2>nul
-git archive -v --format=zip --prefix=mutagen_v1.23/ --output=../mutagen_v1.23.zip HEAD
+del /q ../mutagen_v1.24.zip 2>nul
+git archive -v --format=zip --prefix=mutagen_v1.24/ --output=../mutagen_v1.24.zip HEAD
