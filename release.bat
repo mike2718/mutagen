b1eee05307fa21b09e9538307594ad7ca4d15522
Mike Akiba <mike2718@gmail.com>
2022-03-24 02:39:44 +0800

    修正翻译，修正readme

--- release.bat
+++ release.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d "%~dp0"
-del /q ../mutagen_v1.24.zip 2>nul
-git archive -v --format=zip --prefix=mutagen_v1.24/ --output=../mutagen_v1.24.zip HEAD
+del /q ../mutagen.zip 2>nul
+git archive -v --format=zip --prefix=mutagen/ --output=../mutagen.zip HEAD
+
