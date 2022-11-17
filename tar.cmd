951e861468770a34ae1c2a5b2427440761254c9f
Mike Akiba <mike2718@gmail.com>
2022-11-18 00:01:44 +0800

    更新打包批处理

--- tar.cmd
+++ tar.cmd
@@ -2,4 +2,5 @@
 cd /d "%~dp0"
 del /q ../mutagen.zip 2>nul
 git archive -v --format=zip --prefix=mutagen/ --output=../mutagen.zip HEAD
-
+git archive -v --format=tar.zst --prefix=mutagen/ --output=../mutagen.tar.zst HEAD
+pause
