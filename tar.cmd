1bc532d80eb6800c5f788bc175fed35d942c190e
Mike <mike2718@gmail.com>
2025-03-06 01:38:36 +0800

    移除不需要的文件

--- tar.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-del /q ../mutagen.zip 2>nul
-git archive -v --format=zip --prefix=mutagen/ --output=../mutagen.zip HEAD
-git archive -v --format=tar.zst --prefix=mutagen/ --output=../mutagen.tar.zst HEAD
-pause
